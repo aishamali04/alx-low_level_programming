6edb32f4f8c4d9187d09c39386de97bef6dcd863
Aisha Ali <alikumo04@gmail.com>
2022-10-13 13:01:12 -0700

    2, 3 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * Entry-point: main
+ *   *
+ *    *Return zero if no errors, otherwise return non-zero value.
+ *     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
