3caf0c21aa2885b419d169203a617f34a919c1a3
Aisha Ali <alikumo04@gmail.com>
2022-10-14 09:41:09 -0700

    correct
    i

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * Entry-point: main
- *   *
- *    *Return zero if no errors, otherwise return non-zero value.
- *     */
+ *  
+ * Return zero if no errors, otherwise return non-zero value.
+ *    */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
