4cd4e43a60fbea4d583d3baa426ff6d49702d972
Aisha Ali <alikumo04@gmail.com>
2022-10-14 10:18:57 -0700

    changes to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>                                                                           
-/*                                                                                       
- *  * main: this serves as the main entry point for the program                                                                     
- *  *                                                                                 
- *  * Return: this returns zero if no errors, otherwise returns a non-zero value.                            
- *  * 
- * */                                                                                    
+/**                                                                                       
+ * main: this serves as the main entry point for the program                                  *                                                                                 
+ *  * Return: this returns zero if no errors, otherwise returns a non-zero value.             * 
+ */
 int main(void)                                                                               
 {
 	puts("\"Programming is like building a multilingual puzzle");                               
