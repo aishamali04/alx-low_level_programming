2a438dea5ff88d846fdd05d6c1ae3416f6848cef
Aisha Ali <alikumo04@gmail.com>
2022-10-14 09:58:01 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main- This serves as the entry point for the program
- *  
- * Return: this returns zero if no errors, otherwise it returns a non-zero value.
- *    */
+ * Entry-point: main
+ *   *
+ *    *Return zero if no errors, otherwise return non-zero value.
+ *     */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
