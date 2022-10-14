935c3570603f22ad39772675691fd5008f9e75b9
Aisha Ali <alikumo04@gmail.com>
2022-10-14 09:56:42 -0700

    4-puts.c betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * Return: this returns zero if no errors, otherwise it returns a non-zero value.
  *    */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
