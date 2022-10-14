ad54a440794c8416c6ae4c753b89940a35c91dcf
Aisha Ali <alikumo04@gmail.com>
2022-10-14 10:13:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>                                                                           
+/*                                                                                       
+ *  * main: this serves as the main entry point for the program                                                                     
+ *  *                                                                                 
+ *  * Return: this returns zero if no errors, otherwise returns a non-zero value.                            
+ *  * 
+ * */                                                                                    
+int main(void)                                                                               
+{
+	puts("\"Programming is like building a multilingual puzzle");                               
+		return (0);                                                                                  
+}
