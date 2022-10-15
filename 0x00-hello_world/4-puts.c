14acdfaab67662f98d2dd44b1aed5d3193b94f2f
Aisha Ali <alikumo04@gmail.com>
2022-10-15 14:15:46 -0700

    use puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * main : this is the entry point for the program
  *
- * return: this is the exit point. It returns zero if positive and non-zero value if negative
- *
- * */
+ * return: this returns zero if positive and non-zero value if negative
+ */
 
-int main (void) {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+int main(void) {
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
