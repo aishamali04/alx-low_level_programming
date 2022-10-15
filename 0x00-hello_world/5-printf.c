84a25c15361b6fa10ce6790daebc62fb84c0b599
Aisha Ali <alikumo04@gmail.com>
2022-10-15 14:41:12 -0700

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: Entry point
+ *
+ * Return: zero means success
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
