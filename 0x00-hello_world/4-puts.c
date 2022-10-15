055eae38c8f98002459772a7a708793ea5a53091
Aisha Ali <alikumo04@gmail.com>
2022-10-15 14:31:09 -0700

    short description for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 /**
- * Description: use puts to print text
- * 
  * main : this is the entry point for the program
  *  
  * Return: this returns zero if positive and non-zero value if negative
