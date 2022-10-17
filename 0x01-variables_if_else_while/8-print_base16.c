2717c0235a6daa8f5c0dbcf65789518eb8b7448b
Aisha Ali <alikumo04@gmail.com>
2022-10-17 06:35:56 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main: entry point
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int x;
+	char hexvals [] = "123456789abcdef";
+
+	for (x = '0'; x <= '16'; x++)
+	{
+		putchar(hexvals[x]);
+	}
+	putchar('\n');
+	return (0);
+}
