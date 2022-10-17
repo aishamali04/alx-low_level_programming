558112f0a739dfa05ba8dedb2a43082928445b71
Aisha Ali <alikumo04@gmail.com>
2022-10-17 06:56:43 -0700

    print combo of single digit nums

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main: entry point
+ * Return: 0 is successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar (x);
+		if (x == 9)
+		{
+			putchar (',');
+		}
+		else
+		{
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
