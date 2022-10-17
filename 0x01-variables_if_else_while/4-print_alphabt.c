d47a5fb1599e9b876797c01c47234f2427c343d8
Aisha Ali <alikumo04@gmail.com>
2022-10-17 03:26:31 -0700

    print lowercase except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,12 +2,13 @@
 /**
  * main: entry point
  * Return: 0 if successful
- */
+ * Description: print lowercase letters except e and q
+*/
 int main(void)
 {
 	char x;
 
-	for (x = 'a'; x <='z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		if (x == 'e' || x == 'q')
 			continue;
