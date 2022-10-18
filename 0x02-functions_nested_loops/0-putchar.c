e57e2c594db6d79b7727d294cb3bf2b8e6823b5e
Aisha Ali <alikumo04@gmail.com>
2022-10-18 06:20:04 -0700

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main : Entry point for program
  * Return : 0 if successful
  */
 int main(void)
 {
-	printf("_putchar/n");
+	putchar("/_putchar/n");
 	return (0);
 }
