a5947ba3808874efec79a8b61e1f43150a96601b
Aisha Ali <alikumo04@gmail.com>
2022-10-18 06:30:26 -0700

    include header file main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * main : Entry point for program
  * Return : 0 if successful
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/**
+ * This is the header file
+ * It contains all function declarations
+ */
+int main(void)
+{
+	putchar("/_putchar/n");
+}
