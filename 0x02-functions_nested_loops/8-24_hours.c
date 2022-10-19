1bca0be4d3778fe8a8a656fa5d3798053e67fe7a
Aisha Ali <alikumo04@gmail.com>
2022-10-19 14:55:52 -0700

    hours of day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,22 +5,27 @@
 void jack_bauer(void)
 {
 /** initialize minutes, hours */
+	int minute;
 	int minutes;
+	int hour;
 	int hours;
-	int time;
-	int count;
 
-	for (count = 0; count <= 2; count++)
+	for  (hour >= 0; hour <= 2; hour++)
 	{
-		for  (hours >= 0; hours <= 23; hours++)
+		for (hours >= 0; hours <= 9; hours++)
 		{
-			for (minutes >= 0; minutes <= 59; minutes++)
+			for (minute >= 0; minute <= '5'; minute++)
 			{
-				if (count == 2)
+				for (minutes >= 0; minutes <= 9; minutes++)
+				{
+					if (hour == 2 && hours > 3)
 				{
 					break;
 				}
+				_putchar (hour + '0');
 				_putchar (hours + '0');
+				_putchar (':');
+				_putchar (minute + '0');
 				_putchar (minutes + '0');
 				_putchar('\n');
 			}
