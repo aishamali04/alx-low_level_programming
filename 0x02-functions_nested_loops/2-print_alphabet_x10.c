509d83f3406994b9c841b4b1257d13dafc61e597
Aisha Ali <alikumo04@gmail.com>
2022-10-19 06:07:14 -0700

    fix nested loop arrangement

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@ void print_alphabet_x10(void)
 	char x;
 	int size;
 
-	for (x = 'a'; x <= 'z'; x++)
+	for (size = 0; size < 10; size++)
 	{
-		for (size = 0; size < 10; size++)
+		for (x = 'a'; x <= 'z'; x++)
 		{
 			_putchar (x);
 		}
