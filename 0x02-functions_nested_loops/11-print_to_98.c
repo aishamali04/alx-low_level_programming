90fa9bf588d55aa141e35004b8064463a74b8904
Aisha Ali <alikumo04@gmail.com>
2022-10-20 03:24:32 -0700

    print natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,21 @@
  */
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
+	if (n >= 0)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else (n < 0)
 	{
 		printf("%d, ", n);
 	}
