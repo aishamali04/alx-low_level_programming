8ed454a50c4ff43c8291117d75e34af4884cef57
Aisha Ali <alikumo04@gmail.com>
2022-10-20 02:33:51 -0700

    prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		y = x * 9;
+	}
+	_putchar (y + '0');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,7 @@ extern int print_sign(int n);
 extern int _abs(int);
 extern int print_last_digit(int);
 extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
 #endif
