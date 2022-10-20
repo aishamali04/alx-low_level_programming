66db98e8f8d5f0d4836235a1d55106fd84e2d67f
Aisha Ali <alikumo04@gmail.com>
2022-10-20 02:51:06 -0700

    print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - print natural nums upto 98
+ * @n: variable
+ * Return: 0 is success
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar (n ', ');
+	}
+}
