a28f71a838f8389383630120ae5c1d0692bc1705
Aisha Ali <alikumo04@gmail.com>
2022-10-25 09:08:31 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,13 +9,13 @@ void _puts(char *str)
 {
 	int str_count = 0;
 
-	while (count >= 0)
+	while (str_count >= 0)
 	{
-		if (str[count] == '\0')
+		if (str[str_count] == '\0')
 		{
 			_putchar('\n');
 		}
-		_putchar(str[count]);
-		count++;
+		_putchar(str[str_count]);
+		str_count++;
 	}
 }
