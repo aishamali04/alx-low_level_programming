7247cbf04f9c4dda6030c4d6c7bd901122b8098c
Aisha Ali <alikumo04@gmail.com>
2022-10-29 07:24:09 -0700

    reverses an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,12 @@ void reverse_array(int *a, int n)
 	int y;
 	int temp;
 
-	for ( x = 0; x < n - 1; x++)
+	for (x = 0; x < n - 1; x++)
 	{
 		for (y = x + 1; y > 0; y--)
 		{
 			temp = *(a + y);
-			*(a + y) = *(a + (y -1));
+			*(a + y) = *(a + (y - 1));
 			*(a + (y - 1)) = temp;
 		}
 	}
