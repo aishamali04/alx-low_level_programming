9f09b5b59637e5627121151fcc2b08baa9ad0cc1
Aisha Ali <alikumo04@gmail.com>
2022-10-29 06:50:36 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination variable
+ * @src: source variable
+ * @n: number of bytes
+ * Return: pointer to destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0', x++)
+		dest[x] = src[x];
+	for (; x < n; x++)
+		dest[x] = '\0';
+
+	return (dest);
+}
