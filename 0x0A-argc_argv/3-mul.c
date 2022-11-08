cb16f32450dbed35287cd8d576a615f0a808a815
Aisha Ali <alikumo04@gmail.com>
2022-11-08 05:50:07 -0800

    multiply two numbers and check arg count"

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main method
+ * @argc: arg count
+ * @argv: vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 1;
+	int y;
+
+	if (argc == 3)
+	{
+		for (y = 1; y < argc, y++)
+			x = x * strtol(argv[i], NULL, 10);
+		printf("%d\n", x);
+	}
+	else
+	{
+		printf("Errot\n");
+		return (1);
+	}
+	return (0);
+}
