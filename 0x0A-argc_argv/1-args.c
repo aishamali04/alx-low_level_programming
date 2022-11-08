5cf4cd51fb53884503dd1535cf2ed86fa58cde39
Aisha Ali <alikumo04@gmail.com>
2022-11-08 05:36:13 -0800

    print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,14 +5,14 @@
  * main - main method
  * @argc: argument count
  * @argv: argument vector
- * @Return: 0
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-
-	printf("%s\n", argv[0]);
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
 
 	exit(EXIT_SUCCESS);
 }
